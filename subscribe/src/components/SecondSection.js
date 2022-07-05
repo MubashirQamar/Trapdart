28753ad04faaaa0fe539f5acc9f42f26c05e2f2e
fazalpluton <95477050+fazalpluton@users.noreply.github.com>
2022-07-05 18:36:26 +0500

    fazal

--- subscribe/src/components/SecondSection.js
+++ subscribe/src/components/SecondSection.js
@@ -7,9 +7,10 @@ function SecondSection()
     const [email,setEmail] = useState('');
     const [error,setError] = useState('');
     const [success,setSuccess] = useState('');
-    
+    const [isclick,setIsclick] = useState(false);
     const Subscribe = async (e) => {
         e.preventDefault();
+        setIsclick(true);
         await axios.post(process.env.REACT_APP_SUBSCRIPTION_API_URL+'subscribe', {
             email:email,
         },{headers:{
@@ -20,9 +21,11 @@ function SecondSection()
             setTimeout(() => {
             setSuccess('');
             }, 3000);
+            setIsclick(false);
         })
         .catch(function (error) {
-            setError(error.message);
+            setError(JSON.parse(error.request.response))
+            setIsclick(false);
         });
     }
     return <>
@@ -39,13 +42,14 @@ function SecondSection()
         <div className="join-section">
             <p>Join our mailing list for the latest news</p>
             <div>
-            <input className="custom-input" value={email} onChange={(e)=>setEmail(e.target.value)} type="email" required placeholder="Your email address"/>
+            <input className="custom-input" value={email} onChange={(e)=>setEmail(e.target.value)} type="email" required  placeholder="Your email address"/>
+            
             {
-                error ? <p className="text-danger">{error}</p> : success ? <p className="text-success">{success}</p> : ''
+                error ? <p className="text-danger text-center">{error.email}</p> : success ? <p className="text-success">{success}</p> : ''
             }
             
             </div>
-            <button className="custom-btn sm-secondary-btn">Sign Up</button>
+            <button className="custom-btn sm-secondary-btn" disabled={isclick ?  true: false}>{isclick ? 'Loading...' : 'Sign Up'}</button>
         </div>
         </form>
         </div>
